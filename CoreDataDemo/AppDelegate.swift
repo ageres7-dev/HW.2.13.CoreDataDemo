1289bb167764788f84aa0bb21207b84b85633d8a
ageres7-dev <66694244+ageres7-dev@users.noreply.github.com>
2020-11-25 17:16:18 +0500

    Убрал черновики

--- CoreDataDemo/AppDelegate.swift
+++ CoreDataDemo/AppDelegate.swift
@@ -23,41 +23,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     
-    /*
-    // MARK: - Core Data stack
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "CoreDataDemo")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    */
-    
-    
     func applicationWillTerminate(_ application: UIApplication) {
-//        saveContext()
         StorageManager.delegate.saveContext()
     }
-/*
-    // MARK: - Core Data Saving support
-    func saveContext() {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-    
-    */
-    
 
 }
 
